76e1ca57674a20a2821a6ae4987b85035bbad016
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-10-04 11:15:22 +0200

    CMake: blaze/spdlog hints to package config

--- cmake/FidesConfig.cmake
+++ cmake/FidesConfig.cmake
@@ -4,8 +4,8 @@ include(CMakeFindDependencyMacro)
 
 include("${CMAKE_CURRENT_LIST_DIR}/FidesTargets.cmake")
 
-find_dependency(spdlog REQUIRED)
-find_dependency(blaze REQUIRED)
+find_dependency(spdlog REQUIRED HINTS "@spdlog_DIR@")
+find_dependency(blaze REQUIRED HINTS "@blaze_DIR@")
 find_dependency(LAPACK REQUIRED)
 
 check_required_components(Fides)
